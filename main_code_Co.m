354c2c96f0d2e624b5da59b20d5b084082b5b3ae
Eric Dammann <ericcdammann@gmail.com>
2024-03-12 18:14:49 -0400

    Initial commit

--- main_code_Co.m
+++ main_code_Co.m
@@ -3,9 +3,9 @@ clc; clear variables;
 
 %% Computational Parameters
 
-years = 100;
-n_iter = 1000;
-scaled_n_iter = n_iter/10;
+years = 150;
+n_iter = 10000;
+scaled_n_iter = n_iter/500;
 t_init = 0;
 t_fin = years*365*24*60*60;
 h = (t_fin-t_init)/n_iter;
@@ -14,8 +14,8 @@ t = t_sec/(60*60*24*365);
 
 %% Initial Conditions
 
-wm_init = 3300;
-wl_init = 6700;
+wm_init = 5000;
+wl_init = 5000;
 hm_init = 0.25;
 hl_init = 3.25;
 wb_init = wl_init+wm_init;
@@ -30,12 +30,12 @@ rhos = 1000;
 P = 12.5*3600*1;
 ws = 0.5*10^-3;
 tcr = 0.1;
-Co = linspace(0.01,0.2,scaled_n_iter);
+Co = linspace(0,0.15,scaled_n_iter);
 wind = 6;
 ka = 2;
 ke = 0.16/(365*24*3600);
 amp = 1.4/2;
-rslr = linspace(1,20,scaled_n_iter);
+rslr = linspace(0,15,scaled_n_iter);
 RSLR = rslr*(10^-3)/(3600*24*365);
 rhom = 1000;
 lamda = 0.0001;
@@ -61,6 +61,14 @@ WB(1,:,:) = wb_init;
 CTOT(1,:,:) = ctot_init;
 CLAB(1,:,:) = clab_init;
 CREF(1,:,:) = cref_init;
+
+%% Behavior
+
+behavior = ones(scaled_n_iter,scaled_n_iter);
+stable = 1;
+fill = 2;
+drown = 3;
+erode = 4;
 
 %% Solving Equations
 
@@ -176,7 +184,21 @@ for j=1:scaled_n_iter % Co
 
             % Foward Euler Method
 
-            if (wb+h*dWBdt)-(fetch+h*dWLdt) <=1 || hm+h*dHMdt>=2*amp || hm+h*dHMdt >= hl+h*dHLdt
+            if hm+h*dHMdt >= hl+h*dHLdt || hm+h*dHMdt >= 2*amp
+                
+                behavior(j,k) = drown;
+
+                break
+
+            elseif (wb+h*dWBdt)-(fetch+h*dWLdt) <=1 && hm+h*dHMdt <= 2*amp
+
+                behavior(j,k) = erode;
+
+                break
+
+            elseif (wb+h*dWBdt) <= (wb+h*dWBdt)-(fetch+h*dWLdt)
+
+                behavior(j,k) = fill;
 
                 break
 
@@ -188,7 +210,7 @@ for j=1:scaled_n_iter % Co
                 WB(l+1,k,j) = wb+h*dWBdt;
                 CTOT(l+1,k,j) = ctot+h*dCTOTdt;
                 CLAB(l+1,k,j) = clab+h*dCLABdt;
-                CREF(l+1,k,j) = cref+h*dCREFdt;
+                CREF(l+1,k,j) = cref+h*dCREFdt*wm;
                 MTOT(l+1,k,j) = mtot+h*mflux*wm; 
 
             end
@@ -220,12 +242,12 @@ f_data = zeros(scaled_n_iter,scaled_n_iter);
 
 for m=1:scaled_n_iter
 
-    a_data(m,:) = WM(n_iter,:,m);
-    b_data(m,:) = WL(n_iter,:,m);
-    c_data(m,:) = HM(n_iter,:,m);
-    d_data(m,:) = HL(n_iter,:,m);
-    e_data(m,:) = CREF(n_iter,:,m);
-    f_data(m,:) = MTOT(n_iter,:,m);
+    a_data(:,m) = WM(n_iter,m,:);
+    b_data(:,m) = WL(n_iter,m,:);
+    c_data(:,m) = HM(n_iter,m,:);
+    d_data(:,m) = HL(n_iter,m,:);
+    e_data(:,m) = CREF(n_iter,m,:);
+    f_data(:,m) = MTOT(n_iter,m,:);
 
 end
 
@@ -288,5 +310,53 @@ ylabel("Reference Sediment Concentration (mg/mL)");
 colormap("turbo")
 cb = colorbar();
 cb.Label.String = "kgs of Carbon";
+
+figure(7)
+
+tiledlayout("flow")
+
+nexttile
+hold on
+plot(t,WM(:,17,3)) % Drown
+plot(t,WM(:,10,8)) % Erode
+plot(t,WM(:,10,15)) % Stable
+plot(t,WM(:,1,14)) % Fill
+title("Width of the Marsh");
+ylabel("Meters");
+legend("DR","RE","Ee","Ef",'Location','northwest')
+hold off
+
+nexttile
+hold on
+plot(t,HM(:,17,3)) % Drown
+plot(t,HM(:,10,8)) % Erode
+plot(t,HM(:,10,15)) % Stable
+plot(t,HM(:,1,14)) % Fill
+title("Depth of the Marsh Below MHW");
+ylabel("Meters");
+legend("DR","RE","Ee","Ef",'Location','northwest')
+hold off
+
+nexttile
+hold on
+plot(t,CREF(:,17,3)) % Drown
+plot(t,CREF(:,10,8)) % Erode
+plot(t,CREF(:,10,15)) % Stable
+plot(t,CREF(:,1,14)) % Fill
+title("Refractory Carbon Pool");
+ylabel("kgs of Carbon");
+legend("DR","RE","Ee","Ef",'Location','northwest')
+hold off
+
+nexttile
+hold on
+plot(t,MTOT(:,17,3)) % Drown
+plot(t,MTOT(:,10,8)) % Erode
+plot(t,MTOT(:,10,15)) % Stable
+plot(t,MTOT(:,1,14)) % Fill
+title("Methane Emissions");
+ylabel("kgs of Carbon");
+legend("DR","RE","Ee","Ef",'Location','northwest')
+hold off
 
 toc
